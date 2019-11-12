49d9c212192c4a201659360c66ba83d9d697b6ad
sol <Sergio.ORTEGA-LOPEZ@ext-ec-europa.eu>
2019-11-12 11:32:26 +0100

    '*'

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Apple',
+    price: 1.5,
+    description: 'A Sweet fruit that many people eat in Belgium'
+  },
+  {
+    name: 'Orange',
+    price: 2,
+    description: 'Coming from spain to make juice mainly'
+  },
+  {
+    name: 'Peer',
+    price: 750,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
